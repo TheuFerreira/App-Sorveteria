a45797c0b670fa5ff88a786969d5794122caaba0
_TheuFerreira <matheus.44300@gmail.com>
2022-05-10 11:45:52 -0300

    Starting Project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AccountPage from './src/views/account/account_page';
+import HomePage from './src/views/home/home_page';
+import NotificationsPage from './src/views/notifications/notifications_page';
+import CartPage from './src/views/cart/cart_page';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen name='Carrinho' component={CartPage}/>
+        <Tab.Screen name='Notificações' component={NotificationsPage} />
+        <Tab.Screen name="Conta" component={AccountPage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
