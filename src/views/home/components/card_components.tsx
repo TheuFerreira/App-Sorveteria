64e6ffd802139539480e44efeb3a847022a7a9bc
Matheus Ferreira <matheus.44300@gmail.com>
2022-05-11 15:21:43 -0300

    Converted card component to button

--- src/views/home/components/card_components.tsx
+++ src/views/home/components/card_components.tsx
@@ -1,11 +1,28 @@
-import { Image, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function CardComponent(props: any) {
 
     return (
-        <View style={{alignItems: 'center', flexGrow: 1, margin: 4}}>
-            <Image source={props.picture} style={{width: 80, height: 80, resizeMode: 'contain'}} />
-            <Text style={{fontFamily: props.fontFamily}}>{props.name}</Text>
-        </View>
+        <TouchableOpacity style={styles.button}>
+            <View style={styles.view}>
+                <Image source={props.picture} style={styles.image} />
+                <Text style={{fontFamily: props.fontFamily}}>{props.name}</Text>
+            </View>
+        </TouchableOpacity>
     );
 }
+
+const styles = StyleSheet.create({
+    button: {
+        flexGrow: 1, 
+        margin: 4
+    },
+    view: {
+        alignItems: 'center'
+    },
+    image: {
+        width: 80, 
+        height: 80, 
+        resizeMode: 'contain'
+    }
+});
