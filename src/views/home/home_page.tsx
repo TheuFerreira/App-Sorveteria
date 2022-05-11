15026b566f732393ceaf8e020e5d14d08b1770ee
Matheus Ferreira <matheus.44300@gmail.com>
2022-05-11 16:16:20 -0300

    Started style of Products in Home Page

--- src/views/home/home_page.tsx
+++ src/views/home/home_page.tsx
@@ -1,6 +1,6 @@
 import { loadAsync } from "expo-font";
 import { useEffect, useState } from "react";
-import { FlatList, StatusBar, Text, View } from "react-native";
+import { FlatList, StatusBar, Text, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 import CardComponent from "./components/card_components";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -13,6 +13,14 @@ const numbers = [
     { id: "05", name: "Milk Shakes", picture: require('../../../assets/imgs/milk_shake.webp') }
 ];
 
+const products = [
+    { id: '1', name: 'Açaí 500ML', price: 21.50 },
+    { id: '2', name: 'Açaí 200ML', price: 13 },
+    { id: '3', name: 'Açaí 700ML', price: 17 },
+    { id: '4', name: 'Açaí 200ML', price: 13 },
+    { id: '5', name: 'Açaí 700ML', price: 17 }
+];
+
 export default function HomePage() {
 
     const [loaded, setLoaded] = useState(false)
@@ -20,6 +28,7 @@ export default function HomePage() {
     useEffect(() => {
         loadAsync({
             Pulang: require('../../../assets/fonts/Pulang.ttf'),
+            FuturaHandwritten: require('../../../assets/fonts/FuturaHandwritten.ttf'),
         }).then(() => {
             setLoaded(true);
         });
@@ -53,6 +62,46 @@ export default function HomePage() {
                     numColumns={3}
                     style={{margin: 8, borderRadius: 8, backgroundColor: 'white', padding: 8}}
                     renderItem={({item}) => <CardComponent name={item.name} picture={item.picture} fontFamily={'Pulang'}/>}
+                />
+            </View>
+
+            <View style={{paddingHorizontal: 8}}>
+                <Text style={{fontSize: 18, fontFamily: 'Pulang'}}>Produtos</Text>
+
+                <FlatList
+                    data={products}
+                    keyExtractor={item => item.id}
+                    numColumns={2}
+                    style={{margin: 8}}
+                    renderItem={({item}) => {
+                        return (
+                            <TouchableOpacity style={{
+                                margin: 4, 
+                                backgroundColor: 'white', 
+                                minHeight: 120, 
+                                borderRadius: 8, 
+                                elevation: 4, 
+                                shadowColor: '#000', 
+                                flexGrow: 1,
+                                shadowOffset: { 
+                                    width: 0, 
+                                    height: 0 
+                                }, 
+                                shadowOpacity: .3, 
+                                shadowRadius: 0,
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                overflow: 'hidden',
+                            }}>
+                                <View style={{flex: 1, backgroundColor: '#ff9934', width: '100%'}}></View>
+                                <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 4}}>
+                                    <Text style={{fontFamily: 'Pulang', fontSize: 16}}>{item.name}</Text>
+                                    <Text style={{fontFamily: 'FuturaHandwritten', fontSize: 12}}>R$ {item.price.toFixed(2).replace('.', ',')}</Text>
+                                </View>
+                            </TouchableOpacity >
+                        );
+                    }}
                 />
             </View>
         </View>
